c300a4fd0fa3a7e8cf9f7d978dd3c14bcab7ac77
Pietro <pietromota@hotmail.com>
2022-07-13 08:49:06 -0300

    Add library to handle with localStorage

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
+import localStorage from '../store';
 import AppModule from './app.module';
 import { initialFakeDb } from './tree/entities/tree.mock';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
+
   localStorage.setItem('treeFromDb', JSON.stringify(initialFakeDb));
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3001, () =>
