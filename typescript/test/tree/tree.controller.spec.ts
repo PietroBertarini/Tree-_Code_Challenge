912105c8ecdf26764d37c93a43a970c9d8bd465c
Pietro <pietromota@hotmail.com>
2022-07-13 08:49:22 -0300

    Refactor: rename api to tree

--- typescript/test/tree/tree.controller.spec.ts
+++ typescript/test/tree/tree.controller.spec.ts
@@ -11,7 +11,6 @@ describe('AppController', () => {
       controllers: [TreeController],
       providers: [TreeService],
     }).compile();
-
     apiController = app.get<TreeController>(TreeController);
   });
 
